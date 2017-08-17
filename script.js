e848c9be6924280e893000b28e199e5c4767ef3f
Naveed Golafshani <MSI\ngolafshani@dev-ca22.msi.com>
2017-08-17 10:22:38 -0400

    Initial upload with basic title, artist, cover functionality

---
+++ script.js
@@ -0,0 +1,40 @@
+function geniusSearch( text )
+{
+    if( !text )
+    {
+        document.getElementById( "helpText" ).style.display = "block";
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(
+        'GET',
+        "https://api.genius.com/search?access_token=" + "<accesstoken>" + "&q=" + text,
+        true
+    );
+    xhr.send();
+
+    xhr.onreadystatechange = function()
+    {
+        if( xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200 )
+        {
+            var obj = JSON.parse( xhr.responseText );
+            document.getElementById( "title" ).innerHTML = "Title: " + obj.response.hits[0].result.title;
+            document.getElementById( "title" ).style.display = "block";
+            document.getElementById( "artist" ).innerHTML = "Artist: " + obj.response.hits[0].result.primary_artist.name;
+            document.getElementById( "artist" ).style.display = "block";
+            document.getElementById( "cover" ).src = obj.response.hits[0].result.song_art_image_thumbnail_url;
+            document.getElementById( "cover" ).style.display = "block";
+        }
+    }
+}
+
+chrome.tabs.executeScript(
+    {
+        code: "window.getSelection().toString();"
+    },
+    function( selection )
+    {
+        geniusSearch( selection[0] );
+    }
+);
