28eb393127907163df6ff140e7c7434eb2b6574c
Naveed Golafshani <ngolafshani@live.ca>
2017-08-24 18:48:06 -0400

    Update options page, clean up code. Add login loading indicator - add login response text - use chrome options ui - add more icon resolutions.

--- options.js
+++ options.js
@@ -1,20 +1,34 @@
 function authenticate()
 {
+    document.getElementById( "loginResponse" ).style.display = "none";
+    document.getElementById( "loader" ).style.display = "block";
+
     var authUrl = "https://api.genius.com/oauth/authorize?client_id=fqHI7GwYDkwht665RsE6Yq0M1CE8UzGLp2EuBN2uFxExtFZI_WTdtelJjNfJ56QD&redirect_uri="
-                  + chrome.identity.getRedirectURL() + "&scope=me&state=1&response_type=token";
+                  + chrome.identity.getRedirectURL() + "&scope=me&response_type=token";
 
     chrome.identity.launchWebAuthFlow(
         { url: authUrl, interactive: true },
         function( responseUrl )
         {
             var accessToken = responseUrl.substring( responseUrl.indexOf( "=" ) + 1, responseUrl.indexOf( "&" ) );
-            chrome.storage.sync.set(
-                { 'accessToken': accessToken },
-                function( result )
-                {
-                    alert( "Authenticated" );
-                }
-            );
+            if( accessToken == "access_denied" )
+            {
+                document.getElementById( "loader" ).style.display = "none";
+                document.getElementById( "loginResponse" ).innerHTML = "Please try again.";
+                document.getElementById( "loginResponse" ).style.display = "block";
+            }
+            else
+            {
+                chrome.storage.sync.set(
+                    { 'accessToken': accessToken },
+                    function( result )
+                    {
+                        document.getElementById( "loader" ).style.display = "none";
+                        document.getElementById( "loginResponse" ).innerHTML = "Login succeeded!";
+                        document.getElementById( "loginResponse" ).style.display = "block";
+                    }
+                );
+            }
         }
     );
 }
--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var accessToken;
-var songRelationships = [ "samples", "sampledIn", "interpolates", "interpolatedBy", "coverOf", "coveredBy", "remixOf", "remixedBy", "liveVersionOf", "performedLiveAs" ];
+var songRelationships = [
+    "samples",
+    "sampledIn",
+    "interpolates",
+    "interpolatedBy",
+    "coverOf",
+    "coveredBy",
+    "remixOf",
+    "remixedBy",
+    "liveVersionOf",
+    "performedLiveAs"
+];
 var searchResults = [];
 var currentResult = 0;
 var internallyLinked = false;
@@ -24,32 +35,50 @@ function clear()
     {
         p[i].style.display = "none";
     }
+
     var details = document.getElementsByTagName( "details" );
     for( var i = 0; i < details.length; i++ )
     {
         details[i].style.display = "none";
         details[i].open = false;
     }
-    var img = document.getElementsByTagName( "img" );
-    for( var i = 0; i < img.length; i++ )
-    {
-        img[i].style.display = "none";
-    }
-    var iframe = document.getElementsByTagName( "iframe" );
-    for( var i = 0; i < iframe.length; i++ )
-    {
-        iframe[i].src = "";
-        iframe[i].style.display = "none";
-    }
+
+    document.getElementById( "cover" ).style.display = "none";
+
+    var spotifyPlayer = document.getElementById( "spotifyPlayer" );
+    spotifyPlayer.src = "";
+    spotifyPlayer.style.display = "none";
 
     document.getElementById( "features" ).innerHTML = "";
     document.getElementById( "album" ).innerHTML = "";
     document.getElementById( "releaseDate" ).innerHTML = "";
     document.getElementById( "producers" ).innerHTML = "";
     document.getElementById( "writers" ).innerHTML = "";
+
     for( var i = 0; i < songRelationships.length; i++ )
     {
         document.getElementById( songRelationships[i] ).innerHTML = "";
+    }
+}
+
+function initialInfo( song )
+{
+    document.getElementById( "title" ).innerHTML = externalLinkBuilder( song.title, song.url );
+    document.getElementById( "title" ).style.display = "block";
+
+    document.getElementById( "artist" ).innerHTML = externalLinkBuilder( song.primary_artist.name, song.primary_artist.url );
+    document.getElementById( "artist" ).style.display = "block";
+
+    document.getElementById( "cover" ).src = song.song_art_image_thumbnail_url;
+    document.getElementById( "cover" ).style.display = "block";
+
+    if( searchResults[ currentResult + 1 ] && !internallyLinked )
+    {
+        document.getElementById( "nextResult" ).style.display = "inline";
+    }
+    if( searchResults[ currentResult - 1 ] && !internallyLinked )
+    {
+        document.getElementById( "previousResult" ).style.display = "inline";
     }
 }
 
@@ -61,6 +90,45 @@ function externalLinkBuilder( text, url )
 function internalLinkBuilder( text, songId )
 {
     return "<div class=\"internalLink\" id=\"" + songId + "\">" + text + "</div>";
+}
+
+function constructLinks()
+{
+    var externalLinks = document.getElementsByClassName( "externalLink" );
+    for( var i = 0; i < externalLinks.length; i++ )
+    {
+        (function()
+        {
+            var ln = externalLinks[i];
+            ln.onclick = function()
+            {
+                chrome.tabs.create( { active: false, url: ln.href } );
+            };
+        })();
+    }
+
+    var internalLinks = document.getElementsByClassName( "internalLink" );
+    for( var i = 0; i < internalLinks.length; i++ )
+    {
+        (function()
+        {
+            var ln = internalLinks[i];
+            ln.onclick = function()
+            {
+                internallyLinked = true;
+                geniusSongInfo( ln.id, true );
+            };
+        })();
+    }
+
+    document.getElementById( "nextResult" ).onclick = function()
+    {
+        geniusSongInfo( searchResults[ ++currentResult ], true );
+    }
+    document.getElementById( "previousResult" ).onclick = function()
+    {
+        geniusSongInfo( searchResults[ --currentResult ], true );
+    }
 }
 
 function geniusSearch( text )
@@ -95,32 +163,14 @@ function geniusSearch( text )
                 return;
             }
 
-            var topHit = json.response.hits[0].result;
-
-            document.getElementById( "title" ).innerHTML = externalLinkBuilder( topHit.title, topHit.url );
-            document.getElementById( "title" ).style.display = "block";
-
-            document.getElementById( "artist" ).innerHTML = externalLinkBuilder( topHit.primary_artist.name, topHit.primary_artist.url );
-            document.getElementById( "artist" ).style.display = "block";
-
-            document.getElementById( "cover" ).src = topHit.song_art_image_thumbnail_url;
-            document.getElementById( "cover" ).style.display = "block";
-
             for( var i = 0; i < json.response.hits.length; i++ )
             {
                 searchResults.push( json.response.hits[i].result.id );
             }
 
-            if( searchResults[ currentResult + 1 ] )
-            {
-                document.getElementById( "nextResult" ).style.display = "inline";
-            }
-            if( searchResults[ currentResult - 1 ] )
-            {
-                document.getElementById( "previousResult" ).style.display = "inline";
-            }
+            initialInfo( json.response.hits[0].result );
 
-            geniusSongInfo( topHit.id, false );
+            geniusSongInfo( searchResults[ currentResult ], false );
         }
         else if( xhr.readyState == XMLHttpRequest.DONE && xhr.status != 200 )
         {
@@ -154,23 +204,7 @@ function geniusSongInfo( songId, isInternal )
 
             if( isInternal )
             {
-                document.getElementById( "title" ).innerHTML = externalLinkBuilder( song.title, song.url );
-                document.getElementById( "title" ).style.display = "block";
-
-                document.getElementById( "artist" ).innerHTML = externalLinkBuilder( song.primary_artist.name, song.primary_artist.url );
-                document.getElementById( "artist" ).style.display = "block";
-
-                document.getElementById( "cover" ).src = song.song_art_image_thumbnail_url;
-                document.getElementById( "cover" ).style.display = "block";
-
-                if( searchResults[ currentResult + 1 ] && !internallyLinked )
-                {
-                    document.getElementById( "nextResult" ).style.display = "inline";
-                }
-                if( searchResults[ currentResult - 1 ] && !internallyLinked )
-                {
-                    document.getElementById( "previousResult" ).style.display = "inline";
-                }
+                initialInfo( song );
             }
 
             var numberOfFeatures = song.featured_artists.length;
@@ -186,6 +220,7 @@ function geniusSongInfo( songId, isInternal )
                 }
                 document.getElementById( "features" ).innerHTML += externalLinkBuilder( song.featured_artists[i].name , song.featured_artists[i].url );
             }
+
             if( numberOfFeatures > 0 )
             {
                 document.getElementById( "features" ).style.display = "inline";
@@ -204,9 +239,9 @@ function geniusSongInfo( songId, isInternal )
             {
                 var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
                 document.getElementById( "releaseDate" ).innerHTML +=
-                monthNames[ parseInt( song.release_date.substring( 5, 7 ) ) - 1 ] + " "
-                + parseInt( song.release_date.substring( 8, 10 ) ) + ", "
-                + song.release_date.substring( 0, 4 );
+                    monthNames[ parseInt( song.release_date.substring( 5, 7 ) ) - 1 ] + " "
+                    + parseInt( song.release_date.substring( 8, 10 ) ) + ", "
+                    + song.release_date.substring( 0, 4 );
                 document.getElementById( "releaseDate" ).style.display = "inline";
                 document.getElementById( "releaseDateTag" ).style.display = "inline";
             }
@@ -234,52 +269,25 @@ function geniusSongInfo( songId, isInternal )
                     document.getElementById( songRelationships[j] ).style.display = "block";
                 }
             }
+
             for( var i = 0; i < song.media.length; i++ )
             {
                 if( song.media[i].provider == "spotify" )
                 {
-                    document.getElementById( "spotifyPlayer" ).src = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A" + song.media[i].url.substring( song.media[i].url.indexOf( 31 ) );
+                    //spotify:track:
+                    document.getElementById( "spotifyPlayer" ).src = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A" + song.media[i].native_uri.substring( 14 );
                     document.getElementById( "spotifyPlayer" ).style.display = "block";
                 }
             }
-        }
 
-        var externalLinks = document.getElementsByClassName( "externalLink" );
-        for( var i = 0; i < externalLinks.length; i++ )
-        {
-            (function()
-            {
-                var ln = externalLinks[i];
-                ln.onclick = function()
-                {
-                    chrome.tabs.create( { active: false, url: ln.href } );
-                };
-            }
-        )();
-    }
-    var internalLinks = document.getElementsByClassName( "internalLink" );
-    for( var i = 0; i < internalLinks.length; i++ )
-    {
-        (function()
-        {
-            var ln = internalLinks[i];
-            ln.onclick = function()
-            {
-                internallyLinked = true;
-                geniusSongInfo( ln.id, true );
-            };
+            constructLinks();
         }
-    )();
+        else if( xhr.readyState == XMLHttpRequest.DONE && xhr.status != 200 )
+        {
+            optionsPrompt();
+            return;
+        }
     }
-    document.getElementById( "nextResult" ).onclick = function()
-    {
-        geniusSongInfo( searchResults[ ++currentResult ], true );
-    }
-    document.getElementById( "previousResult" ).onclick = function()
-    {
-        geniusSongInfo( searchResults[ --currentResult ], true );
-    }
-}
 }
 
 chrome.tabs.executeScript(
